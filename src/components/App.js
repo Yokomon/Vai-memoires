5c16a9109827534f4c006579c9a1cf5e12378040
Marow Macaulay <marowmars@gmail.com>
2019-05-21 22:15:15 +0100

    I have added axios and a modal to the project

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,32 @@
 import React from "react";
+import axios from "axios";
 import Header from "./Header";
 import SearchBar from "./SearchBar";
 import UserCard from "./UserCard";
 import "./CardStyle.scss";
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <Header />
-      <SearchBar />
 
-      <div className="grid">
-        <UserCard />
-        <UserCard />
-        <UserCard />
+class App extends React.Component {
+  onSearchSubmit(term) {
+    axios.get("https://api.unsplash.com/search/photos", {
+      headers: {
+        Authorization: "Client-ID"
+      }
+    });
+  }
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <Header />
+        <SearchBar />
+
+        <div className="grid">
+          <UserCard />
+          <UserCard />
+          <UserCard />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default App;
